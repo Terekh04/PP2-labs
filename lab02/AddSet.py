40b82c3c8ad6c5e0a7888e8c397e287d1a4c2c0e
Tereh04 <dmiterekh04@gmail.com>
2025-01-22 15:19:03 +0500

    Add examples from Add Set Items section

---
+++ lab02/AddSet.py
@@ -0,0 +1,19 @@
+thisset = {"apple", "banana", "cherry"}
+
+thisset.add("orange")
+
+print(thisset)
+#using add
+
+
+
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+
+thisset.update(tropical)
+
+print(thisset)
+#using update
+
+
+
