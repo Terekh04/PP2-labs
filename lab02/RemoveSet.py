8ca587a9d363d6cabd9ed43336df1aa1ed00d119
Tereh04 <dmiterekh04@gmail.com>
2025-01-22 15:20:32 +0500

    Add examples from Remove Set Items section

---
+++ lab02/RemoveSet.py
@@ -0,0 +1,7 @@
+thisset = {"apple", "banana", "cherry"}
+
+thisset.discard("banana")
+
+print(thisset)
+#if element doesn't exist remove() will cause a fail, discard() won't
+#then the same as in list
