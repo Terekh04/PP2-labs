34ec85ebc3192cfee935397e13bfe26a16a888dc
Tereh04 <dmiterekh04@gmail.com>
2025-01-22 15:28:31 +0500

    Add examples from Dictionaries section

---
+++ lab02/Dictionary.py
@@ -0,0 +1,9 @@
+#Dictionaries are used to store data values in key:value pairs.
+#Example:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
+#no index
