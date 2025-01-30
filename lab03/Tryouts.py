54b59e7d9e3dcd679746d938973accd473602699
Tereh04 <dmiterekh04@gmail.com>
2025-01-31 00:52:30 +0500

    Some functions trials

---
+++ lab03/Tryouts.py
@@ -0,0 +1,56 @@
+#histogram function
+def histogram(list1):
+    for i in list1:
+        print('*'*i)
+    return
+histogram([4,9,5])
+print()
+#****
+#*********
+#*****
+histogram([10,5,7,1,24,3])
+print()
+#**********
+#*****
+#*******
+#*
+#************************
+#***
+histogram([1,11,111])
+#*
+#***********
+#***************************************************************************************************************
+
+
+
+#spy function
+def spy_game(nums):
+    num=''
+    for i in nums:
+        if i==0 or i==7:
+            num+=str(i)
+    if '007' in num:
+        print('True')
+        return
+    else:
+        print('False')
+        return
+
+spy_game([8,12,0,12,23,0,3235,980,90,8765,678,98,7654,7,56,7890])
+
+
+
+#reverse function
+def reversedSentence(sentence):
+    reversed=[]
+    ans=""
+    for i in sentence.split():
+        reversed.append(i)
+    for i in reversed[::-1]:
+        ans+=i
+        ans+=" "
+    print(ans)
+    return
+reversedSentence('I like mangos')
+reversedSentence('Je ne parle pas français')
+reversedSentence('Съешь ещё этих мягких французских булок, да выпей чаю')
